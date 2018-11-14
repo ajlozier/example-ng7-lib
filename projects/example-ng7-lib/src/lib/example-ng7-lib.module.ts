10b08737bf32bcdb9390593368ba0bfa5b706804
Aaron Lozier <lozieraj@gmail.com>
2018-11-13 18:26:50 -0600

    add foo component to library

--- projects/example-ng7-lib/src/lib/example-ng7-lib.module.ts
+++ projects/example-ng7-lib/src/lib/example-ng7-lib.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ExampleNg7LibComponent } from './example-ng7-lib.component';
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
-  declarations: [ExampleNg7LibComponent],
+  declarations: [ExampleNg7LibComponent, FooComponent],
   imports: [
   ],
   exports: [ExampleNg7LibComponent]
