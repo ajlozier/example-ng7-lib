429fb284b23768316c5b5478ac68454842b9aa53
Aaron Lozier <lozieraj@gmail.com>
2018-11-13 20:28:58 -0600

    ng-bootstrap dependency; incorporate accordion

--- projects/example-ng7-lib/src/lib/example-ng7-lib.module.ts
+++ projects/example-ng7-lib/src/lib/example-ng7-lib.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ExampleNg7LibComponent } from './example-ng7-lib.component';
 import { FooComponent } from './foo/foo.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [ExampleNg7LibComponent, FooComponent],
-  imports: [
-  ],
-  exports: [ExampleNg7LibComponent, FooComponent]
+  declarations: [ExampleNg7LibComponent, FooComponent, AccordionComponent],
+  imports: [ NgbModule ],
+  exports: [ExampleNg7LibComponent, FooComponent, AccordionComponent]
 })
 export class ExampleNg7LibModule { }
